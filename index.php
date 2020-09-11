ae6bb02952ce4fe361bb1f34565127cbfb31bdbf
mumtaj <mumtaj@gmail.com>
2020-09-12 00:40:35 +0530

    Update index.php
    added zoom inage code

--- index.php
+++ index.php
@@ -4,3 +4,21 @@ if ($_POST) {
     // validate post data
     // send email
 }
+
+?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script>
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  $("#js-hero img").css({
+    width: (100 + scroll/5) +"%"
+  })
+})
+</script>
+  
+<section class="hero-section" id="js-hero">
+    <img src="https://source.unsplash.com/random/1920x1080" alt="Image " />
+</section>
+    
+      
